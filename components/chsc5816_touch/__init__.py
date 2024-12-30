8ec1432e43704a91db212bdb10d10e785b4ffa03
jdohn15 <78576051+jdohn15@users.noreply.github.com>
2024-12-30 11:02:10 -0600

    Delete components/chsc5816_touch/__init__.py

--- components/chsc5816_touch/__init__.py
+++
@@ -1,6 +0,0 @@
-from esphome.components import sensor
-from esphome.const import CONF_ID
-
-CONFIG_SCHEMA = sensor.sensor_schema().extend({
-    cv.GenerateID(): cv.declare_id(CHSC5816TouchComponent),
-})
